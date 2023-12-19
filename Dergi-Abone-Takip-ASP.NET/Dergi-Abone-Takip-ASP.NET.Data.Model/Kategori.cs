f4399814298956d67eed5177d9878d9a7a506b53
Mehmet Mert <combas61@gmail.com>
2023-12-19 13:07:40 +0300

    devam

---
+++ Dergi-Abone-Takip-ASP.NET/Dergi-Abone-Takip-ASP.NET.Data.Model/Kategori.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dergi_Abone_Takip_ASP.NET.Data.Model
+{
+    public class Kategori:BaseEntity
+    {
+        [Required]
+        [Column(TypeName ="varchar")]
+        [MaxLength(50)]
+        public string Ad { get; set; }
+        public virtual List<Dergi> Dergiler {  get; set; }
+    }
+}
